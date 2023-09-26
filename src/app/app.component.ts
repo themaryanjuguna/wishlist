fc0b15b6900e6edf2c846cb80fb0b0629ba1853b
unknown <KONZA-VDI@GROUP9-024.vdi.konza>
2023-09-26 15:03:05 +0300

    wish module created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/models/wishitem';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Get Groceries')
+  ];
   title = 'wishlist';
 }
---
+++ src/shared/models/wishitem.ts
@@ -0,0 +1,7 @@
+export class WishItem {
+  constructor(
+    public wishText : string , public isComplete : boolean = false){
+
+    }
+  
+}
